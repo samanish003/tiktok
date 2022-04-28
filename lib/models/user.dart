446c0308a6c8054284097e1a70fcc65695abf1af
samanish003 <67105849+samanish003@users.noreply.github.com>
2022-04-28 12:56:48 +0530

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -30,34 +30,4 @@ class User {
   }
 }
 
-// import 'package:cloud_firestore/cloud_firestore.dart';
-//
-// class User {
-//   late String name;
-//   late String profilePhoto;
-//   late String email;
-//   late String uid;
-//
-//   User({
-//     required this.name,
-//     required this.uid,
-//     required this.email,
-//     required this.profilePhoto,
-//   });
-//
-//   Map<String, dynamic> toJson() => {
-//         "name": name,
-//         "profilePhoto": profilePhoto,
-//         "email": email,
-//         "uid": uid,
-//       };
-//
-//   static User fromSnap(DocumentSnapshot snap) {
-//     var snapshot = snap.data() as Map<String, dynamic>;
-//     return User(
-//         email: snapshot['email'],
-//         profilePhoto: snapshot['profilePhoto'],
-//         uid: snapshot['uid'],
-//         name: snapshot['name']);
-//   }
-// }
+
